76c5684e9d0e015917ca0fceddfef2784efa389a
Tarek Ahmed <unix.geek2014@gmail.com>
2022-01-26 16:28:38 -0600

    Update memorySieve2.asm

--- memorySieve2.asm
+++ memorySieve2.asm
@@ -3,24 +3,27 @@
 ; email: unix.geek2014@gmail.com
 ; This version of memory sieve will work only if you use assembly file and not included as a shellcode. You can also use it as an inline assembly in a C program.
 
-sub esp, 08h
+
 xor eax, eax
 cdq
-call $ + 5	; get address of next instruction and it will be used as our start address for parsing
-pop eax		; eax = address of next instruction
+
+pop eax	
+push eax
+sub esp, 0x8
 xor ecx, ecx
 checkFirstByte:
 inc ecx
-mov  edx, dword ptr[eax+ecx]	; check byte by byte until we find 0xff
-cmp dl, 0ffh
+mov  edx, dword ptr[eax+ecx]
+cmp dl, 0xff
 jne checkFirstByte
-cmp byte ptr[eax+ecx+1], 15h
+cmp byte ptr[eax+ecx+1], 0x15
 jne checkFirstByte
+jmp foundByte
 
 
 foundByte:
-	mov bl, byte ptr [eax+ecx+5]	; check the byte of it ends with 0
-	cmp bl, 0h
+	mov bl, byte ptr [eax+ecx+5]
+	cmp bl, 0
 	je foundPtr
 	jmp checkFirstByte
 
@@ -28,18 +31,50 @@ foundPtr:
 	xor ebx, ebx
 	mov ebx, dword ptr[eax + ecx + 2]
 	mov edi, [ebx]
-	shr edi, 28	; Moving first byte to the end of the address 0x70000000 --> 0x00000007
-	cmp edi, 7h	
+	shr edi, 28
+	cmp edi, 7
 	je foundPossibleAddr
 	jmp checkFirstByte
 
 
 foundPossibleAddr:
-	mov ebx, [ebx]		; This is a possible Kernel32 address, most likely it is.
+	mov ebx, [ebx]
 	xor edx, edx
-	mov dx, 1001h
-	add edx, 0efffh
+	mov dx, 0x1001
+	add edx, 0xefff
+
+findMZ:
 	sub ebx, edx
 	mov bx, dx
+
+	mov ax, [ebx]
+	cmp ax, 0x5a4d		
+	jne findMZ
 	
-			; You can add more code here from the Crack the Stack method to verify if it's really kernel32
+	mov edi, [ebx + 0x3c]	
+	add edi, ebx
+	mov edi, [edi + 0x78]
+	add edi, ebx
+	mov edi, [edi + 0xc]
+	add edi, ebx
+	add edi, 4
+
+	xor eax, eax
+	push eax
+	push 0x6c6c642e
+	push 0x32334c45
+	mov esi, esp
+
+checkKernel :
+	mov edx, ecx
+	mov ecx, 8
+	cld
+
+	repe cmpsb		
+	cmp ecx, 0
+	jne checkFirstByte
+
+FoundKernel32:
+		
+
+
